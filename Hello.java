ea9039dd765c1d3691d0f04c3bf53d1b5e2ec67b
nj23726 <nikhil.p.jain@zs.com>
2023-08-30 17:16:59 +0530

    hello from function

--- Hello.java
+++ Hello.java
@@ -1,7 +1,12 @@
 public class Hello{
 public static void main(String args[]){
 System.out.println("hello world");
+new Hello().sayHello();
 
+}
+
+public void sayHello(){
+System.out.println("Say hello from function");
 }
 
 }
